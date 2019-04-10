2c60cbd696744a22c72c73b9f9815c1b7dbdb89c
khyatimaru <khyati.maru@gmail.com>
2019-04-10 23:39:13 +0100

    Fetches questions from open Trivia DB API, adds spinner, and highlights correct answer if user input wrong answer

--- game.js
+++ game.js
@@ -3,50 +3,57 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
+const choiceTextElem = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
-
-let questions = [
-    {
-      question: "Inside which HTML element do we put the JavaScript??",
-      choice1: "<script>",
-      choice2: "<javascript>",
-      choice3: "<js>",
-      choice4: "<scripting>",
-      answer: 1
-    },
-    {
-      question:
-        "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      choice1: "<script href='xxx.js'>",
-      choice2: "<script name='xxx.js'>",
-      choice3: "<script src='xxx.js'>",
-      choice4: "<script file='xxx.js'>",
-      answer: 3
-    },
-    {
-      question: " How do you write 'Hello World' in an alert box?",
-      choice1: "msgBox('Hello World');",
-      choice2: "alertBox('Hello World');",
-      choice3: "msg('Hello World');",
-      choice4: "alert('Hello World');",
-      answer: 4
-    }
-  ];
+let questions = [];
   
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then( res => {
+        return res.json();
+    })
+    .then( loadedQuestions => {
+        console.log('loadedQuestions ==', loadedQuestions.results)
+
+        questions = loadedQuestions.results.map( loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+
+            }
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random()* 3) + 1;
+            answerChoices.splice(formattedQuestion.answer -1, 0, 
+                    loadedQuestion.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+            })
+        
+            return formattedQuestion;
+        });
+        
+        startGame();
+    })
+    .catch( err => {
+        console.log(err);
+    });
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
@@ -61,7 +68,7 @@ getNewQuestion = () => {
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    question.innerHTML = currentQuestion.question;
     choices.forEach(choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
@@ -84,10 +91,17 @@ choices.forEach(choice => {
         if(classToApply === 'correct') {
             incrementScore(CORRECT_BONUS);
         }
+        else {
+            highlightCorrectAnswer();
+        }
+    
         selectedChoice.parentElement.classList.add(classToApply);
 
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
+            if(classToApply === 'incorrect') {
+                document.getElementsByClassName('show-correct-answer')[0].classList.remove('show-correct-answer');
+            }
             getNewQuestion();
 
         }, 1000);
@@ -99,4 +113,13 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-startGame();
+highlightCorrectAnswer = () => {
+
+    for (var i=0; i < choiceTextElem.length; i++) {
+        if (choiceTextElem[i].dataset.number == currentQuestion.answer) {
+            choiceTextElem[i].parentElement.classList.add('show-correct-answer');
+            return;
+        }
+        
+    }
+}
